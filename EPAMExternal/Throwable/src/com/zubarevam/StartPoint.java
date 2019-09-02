05024ef92dea5d655505dfe3ac24105eaf621249
zubarevam96 <52414134+zubarevam96@users.noreply.github.com>
2019-09-02 18:45:00 +0300

    Create StartPoint.java

---
+++ EPAMExternal/Throwable/src/com/zubarevam/StartPoint.java
@@ -0,0 +1,25 @@
+package com.zubarevam;
+
+import com.zubarevam.classes.Student;
+import com.zubarevam.communicationwithuser.ConsoleUI;
+import com.zubarevam.tests.TestCreation;
+
+
+public class StartPoint {
+
+    public static void main(String[] args) {
+	StartPoint startPoint = new StartPoint();
+	startPoint.consoleTest();
+    }
+
+    private void consoleTest() {
+        try {
+            TestCreation testCreation = new TestCreation();
+            Student[] students = testCreation.create2Students();
+            ConsoleUI consoleUI = new ConsoleUI();
+            consoleUI.show(students);
+        } catch (Exception e) {
+            System.err.println("Exception in Class \"StartPoint\", method \"consoleTest\"");
+        }
+    }
+}
